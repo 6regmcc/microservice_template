f92f36767f68df6925616fb0686d8cbfdeea4624
Greg McCarthy <6regmcc@gmail.com>
2025-02-11 20:40:46 +0000

    added database confirg

---
+++ microservice_template/config/sqlalchemy/db_config.py
@@ -0,0 +1,32 @@
+import os
+from typing import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine(os.environ.get("DATABASE_URL"))
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+def get_db() -> Generator:
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class Base(DeclarativeBase):
+    def to_dict(self):
+        return {field.name: getattr(self, field.name) for field in self.__table__.c}
+
+
+def db_create_all():
+    # Base.metadata.clear()
+    # Base.metadata.drop_all(bind=engine)
+
+    Base.metadata.create_all(bind=engine, checkfirst=True)
--- microservice_template/main.py
+++ microservice_template/main.py
@@ -4,16 +4,19 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
+from microservice_template.config.sqlalchemy.db_config import db_create_all
+
 load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print(f"current environment is {os.environ.get("ENVIRONMENT")}")
+    db_create_all()
     yield
 
 app = FastAPI(lifespan=lifespan,
               root_path=os.environ.get("ROOT_PATH"),
-              openapi_url=f"/{os.environ.get("ROOT_PATH")}/openapi.json",
+              openapi_url=os.environ.get("OPENAPI_URL")
               )
 
 
